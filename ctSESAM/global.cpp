cd53ce50bba541fe499ba5f10c1cac6db2d42fdf
Oliver Lau <oliver@ersatzworld.net>
2015-09-01 19:25:48 +0200

    v2.0 BETA

--- ctSESAM/global.cpp
+++ ctSESAM/global.cpp
@@ -23,7 +23,7 @@
 
 const QString AppCompanyName = "c't";
 const QString AppName = "Qt SESAM";
-const QString AppVersion = "2.0 ALPHA";
+const QString AppVersion = "2.0 BETA";
 const QString AppURL = "https://github.com/ola-ct/Qt-SESAM";
 const QString AppAuthor = "Oliver Lau";
 const QString AppAuthorMail = "ola@ct.de";
