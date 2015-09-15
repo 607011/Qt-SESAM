ab10bcc1747c6d024731562663f251b7e43f3ec0
Oliver Lau <ola@ct.de>
2015-09-15 11:20:47 +0200

    NSIS bugfixes

--- Qt-SESAM/deploy/make-dist.cmd
+++ Qt-SESAM/deploy/make-dist.cmd
@@ -8,7 +8,7 @@ REM *************************************************************************
 
 SET QTDIR="D:\Qt\5.5\msvc2013\bin"
 SET DESTDIR="QtSESAM-portable"
-SET BUILDDIR="..\..\..\QtSESAM-Desktop_Qt_5_5_0_MSVC2013_32bit-Release\ctSESAM\release"
+SET BUILDDIR="..\..\..\QtSESAM-Desktop_Qt_5_5_0_MSVC2013_32bit-Release\Qt-SESAM\release"
 SET PATH=%PATH%;C:\Program Files\7-Zip;D:\Developer\NSIS\
 SET INSTALLER_GLOB="QtSESAM-*-setup.exe"
 
@@ -43,7 +43,7 @@ COPY /B %QTDIR%\icuin54.dll %DESTDIR% >NUL
 COPY /B %QTDIR%\icuuc54.dll %DESTDIR% >NUL
 COPY /B %QTDIR%\..\plugins\platforms\qminimal.dll %DESTDIR%\platforms >NUL
 COPY /B %QTDIR%\..\plugins\platforms\qwindows.dll %DESTDIR%\platforms >NUL
-COPY /B %BUILDDIR%\QtSESAM.exe %DESTDIR% >NUL
+COPY /B %BUILDDIR%\Qt-SESAM.exe %DESTDIR% >NUL
 COPY /B ..\resources\images\* %DESTDIR%\resources\images >NUL
 COPY /Y NUL %DESTDIR%\PORTABLE >NUL
 
@@ -52,7 +52,7 @@ ECHO Build compressed archives ...
 7z a -tZip -mmt=on %DESTDIR%.zip %DESTDIR%
 
 ECHO Launching installer script ...
-makensis.exe /V4 QtSESAM.nsi
+makensis.exe /V4 Qt-SESAM.nsi
 
 ECHO Generating hash files ...
 HashMaster.exe %INSTALLER_GLOB%
