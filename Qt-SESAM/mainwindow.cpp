a820b55b01a151760400ecd694817e61c0335e62
Oliver Lau <oliver@ersatzworld.net>
2016-01-23 10:03:35 +0100

    Cleanups.

--- Qt-SESAM/mainwindow.cpp
+++ Qt-SESAM/mainwindow.cpp
@@ -393,10 +393,7 @@ MainWindow::MainWindow(bool forceStart, QWidget *parent)
   d->trayIcon.setContextMenu(d->trayMenu);
   d->trayIcon.show();
 
-  ui->domainView->setSelectionMode(QAbstractItemView::ExtendedSelection);
-  ui->domainView->setDragEnabled(true);
-  ui->domainView->setAcceptDrops(true);
-  ui->domainView->setDropIndicatorShown(true);
+  // ui->domainView->setSelectionMode(QAbstractItemView::ExtendedSelection);
   QObject::connect(ui->addGroupPushButton, SIGNAL(pressed()), SLOT(onAddGroup()));
   QObject::connect(ui->domainView, SIGNAL(clicked(QModelIndex)), SLOT(onDomainViewClicked(QModelIndex)));
   QObject::connect(ui->domainView, SIGNAL(doubleClicked(QModelIndex)), SLOT(onDomainViewDoubleClicked(QModelIndex)));
