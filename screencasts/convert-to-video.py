86e00e72fd397da334b04b0fa21a6722755e1d53
Oliver Lau <ola@ct.de>
2016-02-02 13:57:04 +0100

    Updated.

--- screencasts/convert-to-video.py
+++ screencasts/convert-to-video.py
@@ -13,12 +13,13 @@ def main():
     folder = sys.argv[1]
     chdir(folder)
     f = glob('*.wmv')[0]
-    subprocess.call([FFMPEG, '-y', '-i', f, '-an', '-preset', 'veryslow', '-qp', '0', '-movflags', '+faststart', '-vcodec', 'libx264', '-vf', 'fps=10', 'output.mp4'])
-    subprocess.call([FFMPEG, '-y', '-i', f, '-an', '-q:v', '0', '-preset', 'veryslow', '-vcodec', 'libvpx', '-vf', 'fps=10', 'output.webm'])
+    subprocess.call([FFMPEG, '-y', '-i', f, '-an', '-q:v', '3', '-preset', 'veryslow', '-vcodec', 'libvpx', '-vf', 'fps=10', 'output.webm'])
+    subprocess.call([FFMPEG, '-y', '-i', f, '-an', '-q:v', '7', '-preset', 'veryslow', '-vcodec', 'libtheora', '-vf', 'fps=10', 'output.ogv'])
+    subprocess.call([FFMPEG, '-y', '-i', f, '-an', '-q:v', '4', '-preset', 'veryslow', '-vcodec', 'libx264', '-vf', 'fps=10', '-movflags', '+faststart', 'output.mp4'])
     p = subprocess.Popen([FFPROBE, '-of', 'flat=s=_', '-v', 'error', '-show_entries', 'format=duration', 'output.mp4'], stdout=subprocess.PIPE)
     p.wait()
     duration = str(0.75 * float(p.communicate()[0].split('=')[1].rstrip()[1:-1]))
-    subprocess.call([FFMPEG, '-y', '-i', 'output.mp4', '-vframes', '1', '-ss', duration, 'thumbnail.png'])
+    subprocess.call([FFMPEG, '-y', '-i', 'output.mp4', '-vframes', '1', '-ss', duration, 'output.png'])
     chdir('..')
 
 
