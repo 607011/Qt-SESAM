607011/Qt-SESAM

commit 91759fcfa2713e6f0bd4d4ef6a25733909e6363a

Improved `Logger`.

Qt-SESAM/logger.cpp
@@ -26,18 +26,28 @@
 #include <QStandardPaths>
 #include <QFile>
 
+class LoggerPrivate {
+public:
+  LoggerPrivate(void)
+    : enabled(true)
+  { /* ... */ }
+  QFile file;
+  bool enabled;
+};
+
 
 Logger::Logger(void)
-  : mEnabled(true)
+  : d_ptr(new LoggerPrivate)
 {
-  /* ... */
+  setFileName(QString("%1/%2.log").arg(QStandardPaths::writableLocation(QStandardPaths::DataLocation)).arg(AppName));
 }
 
 
 Logger::~Logger()
 {
+  Q_D(Logger);
   log(QString("Logger shutting down ..."));
-  mFile.close();
+  d->file.close();
 }
 
 
@@ -50,25 +60,34 @@ Logger &Logger::instance(void)
 
 void Logger::log(const QString &message)
 {
-  qDebug() << message;
-  if (mEnabled && mFile.isOpen()) {
-    mFile.write(QString("[%1] %2\n")
-                .arg(QDateTime::currentDateTime().toString(Qt::ISODate))
-                .arg(message).toUtf8());
-    mFile.flush();
+  Q_D(Logger);
+  if (d->enabled) {
+    const QByteArray &logMsg = QString("[%1] %2")
+        .arg(QDateTime::currentDateTime().toString(Qt::ISODate))
+        .arg(message).toUtf8();
+    if (d->file.isOpen()) {
+      d->file.write(logMsg);
+      d->file.write("\n");
+      d->file.flush();
+    }
+    else {
+      qDebug().noquote().nospace() << logMsg;
+    }
   }
 }
 
 
 void Logger::setEnabled(bool enabled)
 {
-  mEnabled = enabled;
+  Q_D(Logger);
+  d->enabled = enabled;
 }
 
 
 void Logger::setFileName(const QString &filename)
 {
-  mFile.setFileName(filename);
-  mFile.open(QIODevice::WriteOnly | QIODevice::Append);
+  Q_D(Logger);
+  d->file.setFileName(filename);
+  d->file.open(QIODevice::WriteOnly | QIODevice::Append);
   log(QString("Logger writing to %1").arg(filename));
 }

Qt-SESAM/logger.h
@@ -23,6 +23,9 @@
 #include <QtGlobal>
 #include <QString>
 #include <QFile>
+#include <QScopedPointer>
+
+class LoggerPrivate;
 
 class Logger
 {
@@ -32,15 +35,16 @@ public:
   void setEnabled(bool);
   void setFileName(const QString &);
 
+  Logger(const Logger &) = delete;
+  void operator=(Logger const &) = delete;
+
 private:
   Logger(void);
   ~Logger();
-  QFile mFile;
-  bool mEnabled;
 
-public:
-  Logger(const Logger &) = delete;
-  void operator=(Logger const &) = delete;
+
+  QScopedPointer<LoggerPrivate> d_ptr;
+  Q_DECLARE_PRIVATE(Logger)
 
 };
 

commit 94cfe303108a8ef8441761ba3295e538409fa1dd

Fix compilation on Qt 5.3 from debian jessie
The member function QDebug::nospace() was introduced with Qt 5.4. Work
around compilation with older versions of Qt by not using nospace().

Broken since 91759fcf (Improved `Logger`., 2016-01-18).

Signed-off-by: Thomas Braun <thomas.braun@byte-physics.de>

Qt-SESAM/logger.cpp
@@ -25,6 +25,7 @@
 #include <QDateTime>
 #include <QStandardPaths>
 #include <QFile>
+#include <QtGlobal>
 
 class LoggerPrivate {
 public:
@@ -71,7 +72,11 @@ void Logger::log(const QString &message)
       d->file.flush();
     }
     else {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 4, 0))
       qDebug().noquote().nospace() << logMsg;
+#else
+      qDebug().nospace() << logMsg;
+#endif
     }
   }
 }