3d202fc1dff40b78fc920c8923790e6e809e3aaa
Oliver Lau <ola@ct.de>
2016-05-04 16:44:09 +0200

    Cleanups.

--- libSESAM/password.cpp
+++ libSESAM/password.cpp
@@ -201,36 +201,34 @@ SecureString Password::remix(void)
   d->error = NoError;
   d->errorString.clear();
   BigInt::Rossi v(d->pbkdf2.hexKey().toStdString(), BigInt::HEX_DIGIT);
-  const QString &templ = d->ds.passwordTemplate;
-  for (int i = 0; i < templ.length(); ++i) {
-    QString charSet;
-    const char m = templ.at(i).toLatin1();
+  foreach (QChar c, d->ds.passwordTemplate) {
+    const QString *charSet = Q_NULLPTR;
+    const char m = c.toLatin1();
     switch (m) {
     case 'x':
-      charSet = d->ds.usedCharacters;
+      charSet = &d->ds.usedCharacters;
       break;
     case 'o':
-      charSet = d->ds.extraCharacters;
+      charSet = &d->ds.extraCharacters;
       break;
     case 'a': // fall-through
     case 'A': // fall-through
     case 'n':
-      charSet = TemplateCharacters[m];
+      charSet = &TemplateCharacters[m];
       break;
     default:
       d->error = EmptyTemplateError;
       d->errorString = QString("invalid template character: %1").arg(m);
       return SecureString();
     }
-    const int nChars = charSet.count();
-    if (nChars == 0) {
+    if (charSet == Q_NULLPTR) {
       d->error = EmptyCharacterSetError;
       d->errorString = QString("character set for template character %1 must not be empty").arg(m);
       return SecureString();
     }
-    const BigInt::Rossi Modulus(QString("%1").arg(nChars).toStdString(), BigInt::DEC_DIGIT);
+    const BigInt::Rossi Modulus(QString("%1").arg(charSet->size()).toStdString(), BigInt::DEC_DIGIT);
     const BigInt::Rossi &mod = v % Modulus;
-    d->password.append(charSet.at(int(mod.toUlong())));
+    d->password.append(charSet->at(int(mod.toUlong())));
     v = v / Modulus;
   }
   return d->password;
